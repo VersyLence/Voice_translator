1dacdfac4b67033ca5b508b843e06c3460a12c02
Nikulus <147261419+NikulusWhite@users.noreply.github.com>
2025-08-31 20:32:45 +0500

    Delete Audio_translate.py

--- Audio_translate.py
+++
@@ -1,7 +0,0 @@
-from googletrans import Translator
-
-class Translate():
-    async def translate_text(text):
-        translator = Translator()
-        result = await translator.translate(text, dest='en')
-        return result.text
